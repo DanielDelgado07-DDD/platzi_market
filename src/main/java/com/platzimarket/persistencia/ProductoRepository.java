d7363f21d3cf03811aea12a39b3fd4a546e628cf
Daniel Delgado <danielDelgado.07ddd@gmail.com>
2022-06-06 23:06:54 -0500

    agregando query methods, falta crear 2 query methods

--- src/main/java/com/platzimarket/persistencia/ProductoRepository.java
+++ src/main/java/com/platzimarket/persistencia/ProductoRepository.java
@@ -3,14 +3,23 @@ package com.platzimarket.persistencia;
 import com.platzimarket.persistencia.crud.ProductoCrudRepository;
 import com.platzimarket.persistencia.entity.Producto;
 import java.util.List;
+import java.util.Optional;
 
 /* Clase que usa el repositorio de Spring framework data
 * */
 public class ProductoRepository {
-    
+
     private ProductoCrudRepository productoCrudRepository;
     /*Metodo que obtiene todos los productos*/
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    /*Metodo que obtiene los productos por categoria usando  Query methods*/
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
     }
 }
--- src/main/java/com/platzimarket/persistencia/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzimarket/persistencia/crud/ProductoCrudRepository.java
@@ -1,10 +1,18 @@
 package com.platzimarket.persistencia.crud;
 
 import com.platzimarket.persistencia.entity.Producto;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 /*
 * Interface productoCrudRepository
 * extends CrudRepository<Clase,tipo> Define un monton de metodos que se usan sin necesidad de escribirlos
 */
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+    /*@Query(value = "SELECT * FROM productos  WHERE id_categoria = ?",nativeQuery = true)*/
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
 }
